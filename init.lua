07a427354d4b947daf587ea4b46c7ba7fa147141
Zachary Wheeler <37435869+zachywheeler@users.noreply.github.com>
2023-06-19 19:32:56 -0400

    Initial commit

---
+++ init.lua
@@ -0,0 +1,30 @@
+require "zachy.options"
+require "zachy.keymaps"
+require "zachy.plugins"
+require "zachy.colorscheme"
+require "zachy.cmp"
+require "zachy.lsp"
+require "zachy.telescope"
+require "zachy.gitsigns"
+require "zachy.treesitter"
+require "zachy.autopairs"
+require "zachy.comment"
+require "zachy.nvim-tree"
+require "zachy.bufferline"
+require "zachy.lualine"
+require "zachy.toggleterm"
+require "zachy.project"
+require "zachy.impatient"
+require "zachy.indentline"
+require "zachy.alpha"
+require "zachy.whichkey"
+require "zachy.autocommands"
+require('tabnine').setup({
+  disable_auto_comment=true,
+  accept_keymap="<Tab>",
+  dismiss_keymap = "<C-]>",
+  debounce_ms = 800,
+  suggestion_color = {gui = "#808080", cterm = 244},
+  exclude_filetypes = {"TelescopePrompt"},
+  log_file_path = nil, -- absolute path to Tabnine log file
+})
